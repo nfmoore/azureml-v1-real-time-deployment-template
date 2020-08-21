b49d022834bda7d24f0cf344a1457aec05e4971a
Nicholas Moore <nfmoore@outlook.com>
2020-08-21 15:13:55 +1000

    Update data drift unit test

--- tests/unit/data_drift_setup_test.py
+++ tests/unit/data_drift_setup_test.py
@@ -41,11 +41,13 @@ def test_parse_args_draft():
     assert args.frequency == mock_arguments[19]
 
 
+@patch("src.data_drift_setup.AksWebservice", MagicMock())
 @patch("src.data_drift_setup.Dataset", MagicMock())
 @patch("src.data_drift_setup.Datastore", MagicMock())
 @patch("src.data_drift_setup.DataType", MagicMock())
 @patch("src.data_drift_setup.Workspace", MagicMock())
 @patch("src.data_drift_setup.json", MagicMock())
+@patch("src.data_drift_setup.time", MagicMock())
 @patch("src.data_drift_setup.parse_args",)
 @patch("src.data_drift_setup.DataDriftDetector")
 def test_data_drift_setup(mock_data_drift_detector, mock_parse_args):
